0a92ccd9bf5c0b353539a134426baac656d3b870
Carlos Manuel Alcantara Rodriguez <carlos.ma.rodriguez91@gmail.com>
2023-08-03 21:57:42 -0600

    creating-weather-component-adding-weather-icons

---
+++ src/components/Weather.jsx
@@ -0,0 +1,46 @@
+/* informacion y estructura del clima */
+
+const Weather = ({ weather }) => {
+  console.log({ weather });
+  return (
+    <section className="text-center">
+      I will get a 40,000 USD job
+      <h2>{weather?.name}</h2>
+      <section>
+        {/* section superior */}
+        <section className="bg-white/60 p-2 rounded-2xl">
+          <h4>{weather?.weather[0].description}</h4>
+          <span>{weather?.main.temp}°K</span>
+          <div>
+            <img
+              src={`https://openweathermap.org/img/wn/${weather?.weather[0].icon}@4x.png`}
+              alt=""
+            />
+          </div>
+        </section>
+        {/* section inferior */}
+        <section className="bg-white/60 p-2 rounded-2xl">
+          <article>
+            <div>
+              <img src={"/images/viento.png"} alt="" />
+            </div>
+            <span>{weather?.wind.speed} m/s</span>
+          </article>
+          <article>
+            <div>
+              <img src={"/images/humedad.png"} alt="" />
+            </div>
+            <span>{weather?.main.humidity}%</span>
+          </article>
+          <article>
+            <div>
+              <img src={"/images/pressure.png"} alt="" />
+            </div>
+            <span>{weather?.main.pressure} hPa</span>
+          </article>
+        </section>
+      </section>
+    </section>
+  );
+};
+export default Weather;
